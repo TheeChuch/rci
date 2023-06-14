d924967c0fd178e86d52ec8b036345e39597414a
TheeChuch <79095105+TheeChuch@users.noreply.github.com>
2023-06-14 09:16:33 -0500

    Add files via upload

---
+++ Tkinterwidget.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+# Creating a label widget
+#Change the name of the widget by replacing the text "My Widget"
+myLabel = Label(root, text="My Widget")
+# shoving it onto the screen
+myLabel.pack()
+
+root.mainloop
